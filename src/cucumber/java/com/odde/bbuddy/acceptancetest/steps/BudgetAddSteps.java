cfa4ab8300be0b37581cf87e35735a16f2cc5a57
Zen Chan <zen@cacafly.com>
2016-11-23 11:08:57 +0800

    temp

--- src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/BudgetAddSteps.java
+++ src/cucumber/java/com/odde/bbuddy/acceptancetest/steps/BudgetAddSteps.java
@@ -3,15 +3,13 @@ package com.odde.bbuddy.acceptancetest.steps;
 import com.odde.bbuddy.acceptancetest.data.budget.BudgetRepositoryForTest;
 import com.odde.bbuddy.acceptancetest.driver.UiDriver;
 import com.odde.bbuddy.budget.domain.Budget;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by zbcjackson on 22/11/2016.
@@ -32,11 +30,11 @@ public class BudgetAddSteps {
     }
 
     @Then("^exists budget (\\d+) for \"([^\"]*)\"$")
-    public void exists_budget_for(int arg1, String arg2) throws Throwable {
-        List<Budget> budgets = budgetRepository.findAll();
-        assertEquals(1, budgets.size());
-        assertEquals(arg1, budgets.get(0).getAmount());
-        assertEquals(arg2, budgets.get(0).getMonth());
+    public void exists_budget_for(int arg1, String month) throws Throwable {
+        Budget budget = budgetRepository.findByMonth(month);
+        assertNotNull(budget);
+        assertEquals(arg1, budget.getAmount());
+        assertEquals(month, budget.getMonth());
     }
 
     @Given("^budget (\\d+) for \"([^\"]*)\" already exist$")
--- src/cucumber/resources/features/budget/add.feature
+++ src/cucumber/resources/features/budget/add.feature
@@ -10,4 +10,7 @@ Scenario: Input already exist and unique record
   When Add budget 2000 for "2017-10"
   Then exists budget 2000 for "2017-10"
 
-
+Scenario: Input a new budget following an existing budget
+  Given budget 4000 for "2017-02" already exist
+  When Add budget 1000 for "2017-03"
+  Then exists budget 1000 for "2017-03"
